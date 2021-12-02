8716df9aa095c13c8a58e1b0ee91336a5aa8abc5
sahanavijay <86052836+sahanavijay@users.noreply.github.com>
2021-12-02 21:10:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas;
+var jungle, pc_image, npc_image;
+var bro_image, jungle_image;
+var pc, point, junk,bro;
+
+var vx=0;
+var vy=0;
+var g=0.05;
+
+function preload()
+{ 
+ pc_image = loadImage("player.png");
+ jungle_image = loadImage("Background.png");
+}
+
+function setup() 
+{
+  createCanvas(400,400)
+  pc = createSprite(50,310,100,200);
+  pc.addImage(pc_image);
+  pc.scale=0.1;
+  
+}
+
+function draw() 
+{
+  background(51);
+  image(jungle_image,0,0,0,0);
+
+  drawSprites();
+}
